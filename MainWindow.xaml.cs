ac5981e3d9bad1453f055473343d1866635c72e1
Rikki-Wallis <rikkiwallis2004@gmail.com>
2026-01-09 01:31:44 +1100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Meal_Tracker
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void IngredientsButtonClick(object sender, RoutedEventArgs e)
+        {
+            IngredientsButton.Visibility = Visibility.Collapsed;
+            IngredientsActionPanel.Visibility = Visibility.Visible;
+        }
+
+        private void IngredientsBackClick(object sender, RoutedEventArgs e)
+        { 
+            IngredientsActionPanel.Visibility = Visibility.Collapsed;
+            IngredientsButton.Visibility = Visibility.Visible;
+        }
+    }
+}
